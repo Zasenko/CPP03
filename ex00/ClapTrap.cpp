4eba5d5ea546ea1994a8fecf4757e4e9718d3523
Dmitry Zasenko <dzasenko@c3r3p2.42vienna.com>
2025-07-07 14:42:44 +0200

    start ex00

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,7 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzasenko <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/07 14:35:42 by dzasenko          #+#    #+#             */
+/*   Updated: 2025/07/07 14:35:44 by dzasenko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap() {
-    std::cout << "Hello, world!\n";
+ClapTrap::ClapTrap(std::string name) : _name(name), _hit(10), _energy(10), _damage(0) {}
+
+ClapTrap::ClapTrap(const ClapTrap &copy) {
+
 }
 
 ClapTrap::~ClapTrap() {}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& clapTrap) {
+    if (this != &clapTrap) {
+        _name = clapTrap._name;
+        _energy = clapTrap._hit;
+        _energy = clapTrap._energy;
+        _damage = clapTrap._damage;
+    }
+    return *this;
+}
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzasenko <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/07 14:35:37 by dzasenko          #+#    #+#             */
+/*   Updated: 2025/07/07 14:35:38 by dzasenko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CLAPTRAP_HPP
 #define CLAPTRAP_HPP
 
@@ -6,10 +18,18 @@
 class ClapTrap {
     private:
         std::string _name;
+        int _hit;
+        int _energy;
+        int _damage;
 
     public:
-        ClapTrap();
+        ClapTrap(std::string name);
+        ClapTrap(const ClapTrap &copy);
         ~ClapTrap();
+        ClapTrap& operator=(const ClapTrap& clapTrap);
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
 };
 
 #endif
